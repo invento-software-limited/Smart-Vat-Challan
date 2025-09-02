c6dbc952f925e80d8f374d5f98fdcbe4b7900bb1
Md Omar Faruk <sakib@invento.com.bd>
2025-09-02 14:58:35 +0600

    feat: Add custom fields to POS Profile for branch and retailer details

---
+++ vschallan/vat_challan/doctype/retailer_service_type/retailer_service_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Invento Software Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class RetailerServiceType(Document):
+	pass
