5253ea21776401f1e15c2bf86d60e37e68e712c9
Md Omar Faruk <sakib@invento.com.bd>
2025-11-18 05:23:21 +0000

    refactor(vat_challan): Improve VAT invoice handling
    - Refactor VAT invoice creation and syncing logic.
    - Handle return invoices and syncing for POS invoices.
    - Enhance error logging and status updates.

--- vschallan/vat_challan/hook_functions/vat_invoice.py
+++ vschallan/vat_challan/hook_functions/vat_invoice.py
@@ -1,3 +1,5 @@
+import frappe
+
 from vschallan.vschallan import VATSmartChallan
 
 
@@ -5,3 +7,10 @@ def create_vat_invoice(doc, method=None):
 	vschallan = VATSmartChallan()
 	if not doc.status == "Return":
 		vschallan.create_vat_invoice(doc)
+	else:
+		config = frappe.get_single("POS Vendor Configuration")
+		schedule = config.sync_schedule
+		if schedule == "After Submit":
+			vschallan.return_vat_invoice(doc)
+			vat_invoice = frappe.get_doc("VAT Invoice", {"return_invoice_no": doc.name})
+			vat_invoice.sync_vat_invoice()
